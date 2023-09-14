d834379e9b340e17e3babc5a1737b7888ca38c7a
Himanshu <charlie05puth@gmail.com>
2023-09-14 21:10:58 +0530

    day5 working till now

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,52 +6,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset(
-          "assets/images/obito.png",
-          fit: BoxFit.cover,
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Text(
-          "Welcome",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/obito.png",
+            fit: BoxFit.cover,
           ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    hintText: "Enter username", labelText: "Username"),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    hintText: "Enter password", labelText: "Password"),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                child: Text("Login"),
-                style: TextButton.styleFrom(),
-                onPressed: () {
-                  print("Hi Tarun");
-                },
-              ),
-            ],
+          SizedBox(
+            height: 20.0,
           ),
-        ),
-      ],
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hi Tarun");
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     ));
   }
 }
