cba88c05fa31c9a44d8097b0ed039e2af4b62b1a
Himanshu <charlie05puth@gmail.com>
2023-09-15 15:13:48 +0530

    initial 2 commit

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,21 +1,29 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://rukminim1.flixcart.com/image/850/1000/kg8avm80/mobile/j/f/9/apple-iphone-12-dummyapplefsn-original-imafwg8dkyh2zgrh.jpeg?q=90")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({this.id="", this.name="", this.desc="", this.price=0, this.color="", this.image="i"});
-}
-
-final products = [
   Item(
-      id: "Codepur001",
-      name: "iPhone 12 Pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://rukminim1.flixcart.com/image/850/1000/kg8avm80/mobile/j/f/9/apple-iphone-12-dummyapplefsn-original-imafwg8dkyh2zgrh.jpeg?q=90")
-];
+      {this.id = 0,
+      this.name = "",
+      this.desc = "",
+      this.price = 0,
+      this.color = "",
+      this.image = "i"});
+}
